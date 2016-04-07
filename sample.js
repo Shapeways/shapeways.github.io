b1279cd8d6f59078e2cc32c77c5df3ca94b7e6a5
Matt Boyle <matt@shapeways.com>
2016-04-07 20:27:09 +0000

    Basic button shit

---
+++ sample.js
@@ -0,0 +1,19 @@
+/**
+ * Created with PyCharm.
+ * User: matt
+ * Date: 4/7/16
+ * Time: 7:13 PM
+ */
+
+$(document).ready(function() {
+
+    $("#unhide").click(function() {
+        if($(".hidden-div").is(":hidden")) {
+            $(".hidden-div").show();
+            $(this).text('Hide me quick!');
+        } else {
+            $(".hidden-div").hide();
+            $(this).text("Can't see me!");
+        }
+    });
+});
